fde12408620359c19477ad666bd1ffb95cb5a95d
ARU0007 <aruneshagrahari95@gmail.com>
2024-04-18 16:07:27 +0530

    added final to sub class Student.java.java

--- src/main/java/contact/list/app/Student.java
+++ src/main/java/contact/list/app/Student.java
@@ -1,8 +1,21 @@
 package contact.list.app;
 
-public class Student extends Contact {
+public final class Student extends Contact {
     long universityRollNumber;
     String section;
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "universityRollNumber=" + universityRollNumber +
+                ", section='" + section + '\'' +
+                ", cpi=" + cpi +
+                ", id=" + id +
+                ", name='" + name + '\'' +
+                ", phoneNumber=" + phoneNumber +
+                ", email='" + email + '\'' +
+                '}';
+    }
 
     public Student(long id, String name, long phoneNumber, String email, long universityRollNumber, String section, double cpi) {
         super(id, name, phoneNumber, email);
