b655607b6f9977f11312db100a4d7bb98f2f61ab
ARU0007 <aruneshagrahari95@gmail.com>
2024-04-18 15:44:05 +0530

    create subclass Faculty.java

---
+++ src/main/java/contact/list/app/Faculty.java
@@ -0,0 +1,33 @@
+package contact.list.app;
+
+public class Faculty extends Contact {
+    String departmentName;
+    String designation;
+
+    double salary;
+
+    @Override
+    public String toString() {
+        return "Faculty{" +
+                "departmentName='" + departmentName + '\'' +
+                ", designation ='" + designation + '\'' +
+                ", salary=" + salary +
+                ", id=" + id +
+                ", name='" + name + '\'' +
+                ", phoneNumber=" + phoneNumber +
+                ", email='" + email + '\'' +
+                '}';
+    }
+
+    public Faculty(long id, String name, long phoneNumber, String email, String departmentName, String designation, double salary) {
+        super(id, name, phoneNumber, email);
+        this.departmentName = departmentName;
+        this.designation = designation;
+        this.salary = salary;
+    }
+
+    public Faculty(long id, String name, long phoneNumber, String email) {
+
+        super(id, name, phoneNumber, email);
+    }
+}
